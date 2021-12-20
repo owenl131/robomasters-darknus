db74490886f72aaa3a7e049c988c76542c9882e0
Owen <owenl131@gmail.com>
2021-12-20 15:24:55 +0800

    Update

--- serial_comm.py
+++ serial_comm.py
@@ -1,28 +1,34 @@
 import serial
 import struct
 import os
+import time
+
 
 acm = [x for x in os.listdir('/dev') if x.startswith('ttyACM')][0]
 print(acm)
-ser = serial.Serial('/dev/' + acm, 38400, timeout=1)
+ser = serial.Serial('/dev/' + acm, 38400, timeout=1, write_timeout=1)
 ser.flush()
 
-CODE_FORWARD = b'bl'
-CODE_BACKWARD = b'cl'
-CODE_LEFT = b'dl'
-CODE_RIGHT = b'el'
-CODE_STOP = b'sl'
+CODE_FORWARD = b'bm'
+CODE_BACKWARD = b'cm'
+CODE_LEFT = b'dm'
+CODE_RIGHT = b'em'
+CODE_STOP = b'sm'
 
 
 def read_ticks():
     result = None
     while ser.in_waiting:
-        data = ser.read(size=8)
-        result = struct.unpack('II', data)
+        try:
+            data = ser.readline().decode('utf-8').strip()
+            result = list(map(int, data.split(',')))
+        except:
+            pass
     return result
 
 
 def move(direction):
+    ser.flushInput()
     if direction == 'left':
         ser.write(CODE_LEFT)
     elif direction == 'right':
@@ -33,3 +39,12 @@ def move(direction):
         ser.write(CODE_BACKWARD)
     else:
         ser.write(CODE_STOP)
+
+
+if __name__ == '__main__':
+    ser.reset_input_buffer()
+    ser.reset_output_buffer()
+    print(ser.write(CODE_FORWARD))
+    time.sleep(3)
+    print(ser.write(CODE_STOP))
+
